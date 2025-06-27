bf88a818ed89ffde5f2a2f18641811edae407fe2
sagistarz <novialestarin@gmail.com>
2025-06-27 20:29:09 +0700

    remove L, useEffect, Link cause its unused

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
 "use client";
-import { useEffect } from "react";
-import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import MapWrapper from "./components/aod/MapWrapper";
 import Navbar from "./components/navbar/page";
-import Link from "next/link";
 
 export default function MainPage() {
   return (
