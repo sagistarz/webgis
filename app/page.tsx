6f97ea7fdfeeab1835465db1e2acb4ae96de9a56
sagistarz <novialestarin@gmail.com>
2025-06-28 00:43:09 +0700

    change Navbar.tsx name file

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import "leaflet/dist/leaflet.css";
 import MapWrapper from "./components/aod/MapWrapper";
-import Navbar from "./components/navbar/page";
+import Navbar from "./components/navbar/Navbar";
 
 export default function MainPage() {
   return (
